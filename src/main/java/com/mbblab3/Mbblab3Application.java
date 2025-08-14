01f68134f375fcb82b0f6575832a4b423153497b
Ubuntu <rishi@rishi.5aku4sbxyyhuvm5qhgxreqkl4f.tx.internal.cloudapp.net>
2025-08-14 05:05:50 +0000

    auto karna hai deploy

--- src/main/java/com/mbblab3/Mbblab3Application.java
+++ src/main/java/com/mbblab3/Mbblab3Application.java
@@ -8,7 +8,7 @@ public class Mbblab3Application {
 
 	public static void main(String[] args) {
 		
-		System.out.println("rishi -- raj --- Bnagalore");
+		System.out.println("rishi -- raj --- Bnagalore--123");
 		SpringApplication.run(Mbblab3Application.class, args);
 	}
 
